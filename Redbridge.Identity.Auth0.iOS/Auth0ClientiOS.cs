70332ccb4615c61e6b64474a2873339f656ee4ee
Ben Rose <binarysenator@gmail.com>
2018-03-20 21:20:04 +0000

    * Redbridge.Console.nuspec: Updated nuspec to new version and   excluded now changed auth0 client. For another day....
    * Redbridge.nuspec:
    * project.json:
    * Redbridge.symbols.nuspec:
    * Redbridge.Unity.nuspec:
    * Redbridge.Forms.nuspec:
    * Redbridge.SDK.iOS.nuspec:
    * Redbridge.Unity.symbols.nuspec:
    * Redbridge.SDK.Droid.nuspec:
    * Auth0ClientiOS.cs:
    * Redbridge.Forms.symbols.nuspec:
    * Redbridge.SDK.Windows.nuspec:
    * Redbridge.SDK.iOS.symbols.nuspec:
    * Redbridge.Console.symbols.nuspec:
    * Redbridge.SDK.Droid.symbols.nuspec:
    * Redbridge.Services.WebApi.nuspec:
    * Redbridge.SDK.Windows.symbols.nuspec:
    * Redbridge.EntityFramework.nuspec:
    * Resource.designer.cs:
    * Redbridge.Xamarin.Forms.iOS.nuspec:
    * Redbridge.IntegrationTesting.nuspec:
    * Redbridge.Identity.Auth0.iOS.nuspec:
    * Redbridge.EntityFramework.symbols.nuspec:
    * Redbridge.Services.WebApi.symbols.nuspec:
    * Redbridge.Xamarin.Forms.Droid.nuspec:
    * Redbridge.Identity.Google.iOS.nuspec:
    * Redbridge.Xamarin.Forms.iOS.symbols.nuspec:
    * Redbridge.Identity.Auth0.iOS.symbols.nuspec:
    * Redbridge.IntegrationTesting.symbols.nuspec:
    * Redbridge.Identity.Auth0.Windows.nuspec:
    * Redbridge.Identity.Google.iOS.symbols.nuspec:
    * Redbridge.Xamarin.Forms.Droid.symbols.nuspec:
    * Redbridge.Diagnostics.ApplicationInsights.nuspec:
    * Redbridge.Diagnostics.ApplicationInsights.symbols.nuspec:

--- Redbridge.Identity.Auth0.iOS/Auth0ClientiOS.cs
+++ Redbridge.Identity.Auth0.iOS/Auth0ClientiOS.cs
@@ -1,92 +1,90 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Reactive.Subjects;
-using System.Threading.Tasks;
-using Auth0.SDK;
-using Redbridge.Identity;
-using Redbridge.Security;
-using UIKit;
+//using System;
+//using System.IO;
+//using System.Reactive.Subjects;
+//using System.Threading.Tasks;
+//using Auth0.OidcClient;
+//using Redbridge.Identity;
+//using Redbridge.Security;
 
-namespace Redbridge.Xamarin.Forms.iOS
-{
-	public class Auth0ClientIos : IAuthenticationClient
-	{
-		private readonly Auth0Client _platformAuth0;
-		private Auth0User _userCredentials;
-		private BehaviorSubject<ClientConnectionStatus> _connectionStatus = new BehaviorSubject<ClientConnectionStatus>(ClientConnectionStatus.Disconnected);
+//namespace Redbridge.Xamarin.Forms.iOS
+//{
+//	public class Auth0ClientIos : IAuthenticationClient
+//	{
+//		private readonly Auth0Client _platformAuth0;
+//		private Auth0User _userCredentials;
+//		private BehaviorSubject<ClientConnectionStatus> _connectionStatus = new BehaviorSubject<ClientConnectionStatus>(ClientConnectionStatus.Disconnected);
 
-		public Auth0ClientIos(string domain, string clientId)
-		{
-			_platformAuth0 = new Auth0Client(domain, clientId);
-		}
+//		public Auth0ClientIos(string domain, string clientId)
+//		{
+//			_platformAuth0 = new Auth0Client()domain, clientId);
+//		}
 
-		public ClientConnectionStatus CurrentConnectionStatus => _connectionStatus.Value;
+//		public ClientConnectionStatus CurrentConnectionStatus => _connectionStatus.Value;
 
-		public async Task BeginLoginAsync ()
-		{
-			_connectionStatus.OnNext(ClientConnectionStatus.Connecting);
-			var uiViewController = RootViewControllerFinder.GetCurrent();
-			_userCredentials = await _platformAuth0.LoginAsync(uiViewController);
+//		public async Task BeginLoginAsync ()
+//		{
+//			_connectionStatus.OnNext(ClientConnectionStatus.Connecting);
+//			var uiViewController = RootViewControllerFinder.GetCurrent();
+//			_userCredentials = await _platformAuth0.LoginAsync(uiViewController);
 
-			if (string.IsNullOrWhiteSpace(_userCredentials.Auth0AccessToken))
-				_connectionStatus.OnNext(ClientConnectionStatus.Disconnected);
-			else
-				_connectionStatus.OnNext(ClientConnectionStatus.Connected);
+//			if (string.IsNullOrWhiteSpace(_userCredentials.Auth0AccessToken))
+//				_connectionStatus.OnNext(ClientConnectionStatus.Disconnected);
+//			else
+//				_connectionStatus.OnNext(ClientConnectionStatus.Connected);
 
-		}
+//		}
 
-        public string ClientType => "Auth0";
+//        public string ClientType => "Auth0";
 
-		public IObservable<ClientConnectionStatus> ConnectionStatus
-		{
-			get { return _connectionStatus; }
-		}
+//		public IObservable<ClientConnectionStatus> ConnectionStatus
+//		{
+//			get { return _connectionStatus; }
+//		}
 
-		public bool IsConnected
-		{
-			get { return _userCredentials != null; }
-		}
+//		public bool IsConnected
+//		{
+//			get { return _userCredentials != null; }
+//		}
 
-		public string AccessToken
-		{
-			get { return _userCredentials?.Auth0AccessToken; }
-		}
+//		public string AccessToken
+//		{
+//			get { return _userCredentials?.Auth0AccessToken; }
+//		}
 
-		public string BearerToken
-		{
-			get { return _userCredentials?.IdToken; }
-		}
+//		public string BearerToken
+//		{
+//			get { return _userCredentials?.IdToken; }
+//		}
 
-        public string Username
-        {
-            get { throw new NotImplementedException("You need to crack open the JSON profile and get this information."); }
-        }
+//        public string Username
+//        {
+//            get { throw new NotImplementedException("You need to crack open the JSON profile and get this information."); }
+//        }
 
-		public Task LogoutAsync ()
-		{
-			_platformAuth0.Logout();
-			_userCredentials = null;
+//		public Task LogoutAsync ()
+//		{
+//			_platformAuth0.Logout();
+//			_userCredentials = null;
 
-			if ( _connectionStatus.Value != ClientConnectionStatus.Disconnected )
-				_connectionStatus.OnNext(ClientConnectionStatus.Disconnected);
+//			if ( _connectionStatus.Value != ClientConnectionStatus.Disconnected )
+//				_connectionStatus.OnNext(ClientConnectionStatus.Disconnected);
 
-            return Task.CompletedTask;
-		}
+//            return Task.CompletedTask;
+//		}
 
-        public Task<Stream> SaveAsync()
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<Stream> SaveAsync()
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task<UserCredentials> LoadAsync(Stream stream)
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<UserCredentials> LoadAsync(Stream stream)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public void SetCredentials(UserCredentials credentials)
-        {
-            throw new NotSupportedException("You cannot explicitly set the credentials here at the moment.");
-        }
-    }
-}
+//        public void SetCredentials(UserCredentials credentials)
+//        {
+//            throw new NotSupportedException("You cannot explicitly set the credentials here at the moment.");
+//        }
+//    }
+//}
