92112467b856d0b340b0431f03c90fe0bf865dd2
Ben Rose <binarysenator@gmail.com>
2018-06-14 14:01:41 +0100

    * packages.config: Updated versions and included new filter for 404   page.
    * Redbridge.sln:
    * TesterApp.iOS.csproj:
    * Redbridge.csproj:
    * Redbridge.nuspec:
    * project.json:
    * project.json:
    * Redbridge.symbols.nuspec:
    * Redbridge.Unity.csproj:
    * Redbridge.Unity.nuspec:
    * Redbridge.Forms.csproj:
    * Redbridge.Forms.nuspec:
    * project.json:
    * packages.config:
    * Redbridge.SDK.iOS.nuspec:
    * Redbridge.Console.nuspec:
    * packages.config:
    * packages.config:
    * project.json:
    * packages.config:
    * Redbridge.Unity.symbols.nuspec:
    * Redbridge.SDK.Droid.nuspec:
    * Redbridge.Forms.symbols.nuspec:
    * packages.config:
    * Redbridge.SDK.Windows.nuspec:
    * Redbridge.SDK.iOS.symbols.nuspec:
    * AssemblyInfo.cs:
    * Redbridge.Console.symbols.nuspec:
    * ThemeViewerContentPage.xaml:
    * Redbridge.Services.Owin.csproj:
    * Redbridge.SDK.Droid.symbols.nuspec:
    * packages.config:
    * packages.config:
    * Redbridge.SDK.Windows.symbols.nuspec:
    * Redbridge.Services.WebApi.nuspec:
    * Redbridge.Services.WebApi.csproj:
    * Redbridge.EntityFramework.nuspec:
    * Redbridge.Forms.Tests.csproj:
    * Redbridge.Xamarin.Forms.iOS.nuspec:
    * Redbridge.IntegrationTesting.csproj:
    * Redbridge.Identity.Auth0.iOS.nuspec:
    * Redbridge.Identity.Auth0.iOS.csproj:
    * Redbridge.IntegrationTesting.nuspec:
    * Redbridge.Identity.Google.iOS.nuspec:
    * Redbridge.EntityFramework.symbols.nuspec:
    * Redbridge.Services.WebApi.symbols.nuspec:
    * NotFoundCustomMessageHandler.cs:
    * Redbridge.Identity.Google.iOS.csproj:
    * ApplicationInsights.config:
    * Redbridge.Xamarin.Forms.iOS.symbols.nuspec:
    * Redbridge.Console.Test.csproj:
    * Redbridge.Identity.Facebook.iOS.csproj:
    * Redbridge.IntegrationTesting.symbols.nuspec:
    * Redbridge.Identity.Auth0.Windows.nuspec:
    * Redbridge.Identity.Auth0.Windows.csproj:
    * Redbridge.Identity.Auth0.iOS.symbols.nuspec:
    * packages.config:
    * Redbridge.Identity.Google.iOS.symbols.nuspec:
    * HttpConfigurationFilterExtensions.cs:
    * Redbridge.Diagnostics.ApplicationInsights.nuspec:
    * Redbridge.Diagnostics.ApplicationInsights.csproj:
    * Redbridge.EntityFramework.Tests.csproj:
    * Redbridge.Diagnostics.ApplicationInsights.symbols.nuspec:

--- Redbridge.Services.WebApi/Configuration/HttpConfigurationFilterExtensions.cs
+++ Redbridge.Services.WebApi/Configuration/HttpConfigurationFilterExtensions.cs
@@ -3,6 +3,7 @@ using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
 using Redbridge.Diagnostics;
 using Redbridge.Services.WebApi.Filters;
+using Redbridge.Services.WebApi.Handlers;
 
 namespace Redbridge.Services.WebApi.Configuration
 {
@@ -20,6 +21,7 @@ namespace Redbridge.Services.WebApi.Configuration
             configuration.Filters.Add(new UserNotAuthenticatedExceptionFilter(logger));
             configuration.Filters.Add(new UserNotAuthorizedExceptionFilter(logger));
             configuration.Filters.Add(new LoggingExceptionFilter(logger));
+			configuration.MessageHandlers.Add(new NotFoundCustomMessageHandler());
 
             logger.WriteDebug("Installing exception logger...");
             configuration.Services.Replace(typeof(IExceptionLogger), new UnhandledExceptionLogger(logger));
---
+++ Redbridge.Services.WebApi/Handlers/NotFoundCustomMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Hosting;
+
+namespace Redbridge.Services.WebApi.Handlers
+{
+	public class NotFoundCustomMessageHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+            if (response.StatusCode != HttpStatusCode.NotFound) return response;
+            request.Properties.Remove(HttpPropertyKeys.NoRouteMatched);
+            var errorResponse = request.CreateResponse(response.StatusCode, "Page or resource not found.");
+            return errorResponse;
+        }
+    }
+}
