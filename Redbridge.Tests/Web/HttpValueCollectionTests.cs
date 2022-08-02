6d099984d8a11cb4f012545a3259342ef1aea733
Ben Rose <ben.rose@logitbox.com>
2022-08-02 12:00:03 +0100

    Added tests only

---
+++ Redbridge.Tests/Web/HttpValueCollectionTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Redbridge.Web;
+
+namespace Redbridge.Tests.Web
+{
+    [TestFixture]
+    public class HttpValueCollectionTests
+    {
+        [Test]
+        public void Construct_HttpValueCollection_DefaultConstructor()
+        {
+            var collection = new HttpValueCollection();
+            Assert.AreEqual(0, collection.Count);
+        }
+    }
+}
